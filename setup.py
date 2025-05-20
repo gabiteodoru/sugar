4036ab707e1128c0ef5925f53b4ebd0cd92f5459
gabi <gabiteodoru@gmail.com>
2025-05-20 12:08:38 +0100

    Initial commit

---
+++ setup.py
@@ -0,0 +1,23 @@
+from setuptools import setup, find_packages
+
+setup(
+    name="sugar",
+    version="0.1.0",
+    packages=find_packages(),
+    install_requires=[
+        # List dependencies here
+        # "numpy>=1.20.0",
+    ],
+    author="Gabi Teodoru",
+    author_email="gabiteodoru@gmail.com",
+    description="Gabi's syntactic sugar & other utils",
+    long_description=open("README.md").read(),
+    long_description_content_type="text/markdown",
+    url="https://https://github.com/gabiteodoru/sugar",
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+    python_requires=">=3.10",
+)
